d17c20c92b5d912ad4f09653c081a5d5bb0dc371
HootOwl17 <35872932+HootOwl17@users.noreply.github.com>
2018-02-25 23:07:53 -0600

    Editando un pequeño detalle

--- node index.js
+++ node index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = '517895758:AAHo_YwJVayge-P6GiYYw2ybe8xtwRCGbZQ';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
